6f9fa857d17dc24b6bf314215f6789058208b4a2
Bogdan <bogdya2@gmail.com>
2019-05-16 08:36:29 +0300

    Add detecting of port

--- index.js
+++ index.js
@@ -54,6 +54,12 @@ app.use((error, req, res, next) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Сервер работает на порте 3000.')
+app.set('port', (process.env.PORT || 5000));
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
 });
